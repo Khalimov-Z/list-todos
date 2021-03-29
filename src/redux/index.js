c13ce9398edfbf640bb7c1f0bd8f90ba8f620df5
Khalimov-Z <djordlo077@gmail.com>
2021-03-29 05:17:11 +0300

    add app

---
+++ src/redux/index.js
@@ -0,0 +1,12 @@
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { createLogger } from 'redux-logger/src'
+import reducer from './todos'
+
+const logger =createLogger({
+  diff:true,
+  collapsed:true,
+})
+
+const store =createStore(reducer,applyMiddleware(thunk,logger));
+export default store;
---
+++ src/redux/todos.js
@@ -0,0 +1,64 @@
+const LOAD_START = 'todos/load/start';
+const LOAD_SUCCESS = 'todos/load/success';
+const CHECK_START = 'todos/check/start';
+const CHECK_SUCCESS = 'todos/check/success';
+const REMOVE_START = 'todos/remove/start';
+const REMOVE_SUCCESS = 'todos/remove/success';
+const initialState = {
+  items: [],
+  loading: false,
+};
+
+export default function reducer(state = initialState, action){
+  switch (action.type){
+    case LOAD_START:
+      return{
+
+      }
+    case LOAD_SUCCESS:
+      return {
+
+      }
+    case CHECK_START:
+      return {
+
+      }
+    case CHECK_SUCCESS:
+      return {
+
+      }
+    case REMOVE_START:
+      return {
+
+      }
+    case REMOVE_SUCCESS:
+      return {
+
+      }
+
+    default:
+      return state;
+  }
+}
+export const loadTodos = () => {
+  return (dispatch) => {
+    dispatch({
+      type:LOAD_START
+    });
+  }
+}
+export const removeTodo = (id) => {
+  return (dispatch) => {
+    dispatch({
+      type: REMOVE_START
+    });
+  }
+}
+export const checkTodo = (id,completed) => {
+  return (dispatch) => {
+    dispatch({
+      type: CHECK_START,
+      payload:id,
+    });
+  }
+}
